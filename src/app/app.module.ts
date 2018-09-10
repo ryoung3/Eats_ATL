45a090d1ed9aab1e8c91fddf4f1617ab4c58fc8c
Ronique Young <youngronique09@gmail.com>
2018-09-09 21:49:24 -0500

    added googlemap component. edit landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GooglemapComponent } from './googlemap/googlemap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GooglemapComponent
   ],
   imports: [
     BrowserModule
