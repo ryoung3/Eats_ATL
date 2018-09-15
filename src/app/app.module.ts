d0b81edf8fb017fceaf3dd6d01295cec6a1e9c44
Ronique Young <youngronique09@gmail.com>
2018-09-15 19:11:36 -0400

    added modules for HTTP request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GooglemapComponent } from './googlemap/googlemap.component';
@@ -13,6 +14,7 @@ import { AgmCoreModule } from '@agm/core';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
       apiKey: '',
       libraries: ['places']
