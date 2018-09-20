956c61cfae20aadf2ffe634f7708592841b794b8
Ronique Young <youngronique09@gmail.com>
2018-09-20 00:42:30 -0400

    added resturants objects array. developed method to display restaurants object array

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,8 +1,43 @@
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class restaurantsService{
 
-
-    constructor(){
-
+    getRestaurants(){
+        return this.restaurants;
     }
+ restaurants = [{
+    name: "Chruches",
+    address: "1234 Jackson Street",
+    description: "chicken joint",
+    rating: "5 stars",
+    customerReview: "good food"
+  },
+
+  {
+    name: "Blondies",
+    address: "4567 James Street",
+    description: "dessert joint",
+    rating: "5 stars",
+    customerReview: "good food"
+  },
+
+  {
+    name: "Chitpole",
+    address: "2389 Peachtree Street",
+    description: "mexican food",
+    rating: "5 stars",
+    customerReview: "good food"
+  },
+
+  {
+    name: "Atlanta Tech Village",
+    address: "3456 Piedmont Road",
+    description: "innovation center",
+    rating: "5 stars",
+    customerReview: "great place to learn"
+  }];
+
+    
+    
 }
