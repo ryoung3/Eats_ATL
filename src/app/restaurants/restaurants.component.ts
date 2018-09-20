b0abad5dccf644f0137263366a8cebc867704e5f
Ronique Young <youngronique09@gmail.com>
2018-09-20 00:41:15 -0400

    added restaurant objects from restaurants service

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { restaurantsService } from './restaurants.service';
 
 @Component({
   selector: 'app-restaurants',
@@ -6,43 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurants.component.css']
 })
 export class RestaurantsComponent implements OnInit {
+    restaurants: any[]
 
-  restaurants = [{
-    name: "Chruches",
-    address: "1234 Jackson Street",
-    description: "chicken joint",
-    rating: "5 stars",
-    customerReview: "good food"
-  },
-
-  {
-    name: "Blondies",
-    address: "4567 James Street",
-    description: "dessert joint",
-    rating: "5 stars",
-    customerReview: "good food"
-  },
-
-  {
-    name: "Chitpole",
-    address: "2389 Peachtree Street",
-    description: "mexican food",
-    rating: "5 stars",
-    customerReview: "good food"
-  },
-
-  {
-    name: "Atlanta Tech Village",
-    address: "3456 Piedmont Road",
-    description: "innovation center",
-    rating: "5 stars",
-    customerReview: "great place to learn"
-  }]
-    
-  
-    constructor() { }
+    constructor(private restaurantsService: restaurantsService){
+        
+     }
 
   ngOnInit() {
+    this.restaurants = this.restaurantsService.getRestaurants();
   }
 
 }
