e4ad2b5a02d4f2dceb46173459dfaf3cfc9421eb
Ronique Young <youngronique09@gmail.com>
2018-09-23 23:38:21 -0400

    added import for google markers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AgmMarker } from '@agm/core';
 
 import { AppComponent } from './app.component';
 import { GooglemapComponent } from './googlemap/googlemap.component';
@@ -18,6 +19,7 @@ import { restaurantsService } from "src/app/restaurants/restaurants.service";
     BrowserModule,
     HttpClientModule,
     NgbModule.forRoot(),
+    AgmMarker,
     AgmCoreModule.forRoot({
       apiKey: '',
       libraries: ['places']
