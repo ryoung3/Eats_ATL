120784e8b2de66f87ce20ee24c85ac8a29e0d448
Ronique Young <youngronique09@gmail.com>
2018-10-07 23:15:53 -0400

    removed temporary restaurants array

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -6,37 +6,7 @@ import { HttpClient } from '@angular/common/http';
 export class restaurantsService{
   
   yelp_URL: String;
-  restaurants: any[] = [{
-    name: "Chruches",
-    address: "1234 Jackson Street",
-    description: "chicken joint",
-    rating: "5 stars",
-    customerReview: 'good food'
-  },
-
-  {
-    name: "Blondies",
-    address: "4567 James Street",
-    description: "dessert joint",
-    rating: "5 stars",
-    customerReview: "good food"
-  },
-
-  {
-    name: "Chitpole",
-    address: "2389 Peachtree Street",
-    description: "mexican food",
-    rating: "5 stars",
-    customerReview: "good food"
-  },
-
-  {
-    name: "Atlanta Tech Village",
-    address: "3456 Piedmont Road",
-    description: "innovation center",
-    rating: "5 stars",
-    customerReview: "great place to learn"
-  }];
+  restaurants: any[];
   constructor(private http: HttpClient){
     
   }
